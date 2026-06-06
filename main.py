35378419407890ee98b4ee60b422e4ade2a61ed6
Tobias Grundmann <tobias.grundmann@tu-dortmund.de>
2026-06-06 22:48:21 +0200

    2

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
+# folium docs: https://python-visualization.github.io/folium/latest/index.html
 import pandas as pd
-from pathlib import Path
-import re
 import folium
-
-# First, view data and sort into data frames for processing
-# Issue: Main files. Unclear which file/sensor corresponds to which point on the given map
-# Using regex to match the button ids is difficult
-# iButton_df = pd.DataFrame()
-# iButton_folder = Path("Data for students/iButton data")
-# csv_files = list(iButton_folder.rglob("*.csv"))
-# print(csv_files)
-#
-# for file_path in csv_files:
-#     year = re.search(r'.*\\(\d{4})\\.*', str(file_path)).group(1)
-#
-#     match year:
-#         case "2026":
-#             date = re.search(r'.*\\(\d{1,2}.\d{1,2}.\d{2,4})\\.*', str(file_path)).group(1)
-#             id = re.search(r'.*\\d{2,4}\\(\d{1,2})([TtHhOoIi])', str(file_path)).group(1)
-#             print(id)
-#
 
 ## Sensor Data
 sensor_location = pd.read_excel("Data for students/nestbox_coordinates.xlsx")
-
 
 ## Map Stuff
 # Bounding box for relevant map section of TU Dortmund
@@ -55,7 +35,6 @@ for sensor in sensor_location.itertuples():
     sensor_type = sensor.sensor_type
 
     kw = {"prefix": "fa", "color": "green", "icon": "arrow-up"}
-
     icon = folium.Icon(angle=sensor_rot_number, **kw)
     folium.Marker(location=[sensor_lat, sensor_lon], icon=icon, tooltip=str(sensor_rot_text)).add_to(tu_map)
 
